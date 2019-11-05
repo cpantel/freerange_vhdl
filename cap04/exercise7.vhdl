14e70a2206e3716d84c11d060243b5b88a10368a
Carlos Pantelides <carlos.pantelides@gmail.com>
2019-11-05 09:10:42 -0300

    gtkwave ok

--- cap04/exercise7.vhdl
+++ cap04/exercise7.vhdl
@@ -1,4 +1,4 @@
--- Provide a VHDL model of a 3:8 decoder using conditional signal assignment and selected signal assignment; consider the decoder’s outputs to be active-low.
+-- Provide a VHDL model of a 3:8 decoder using conditional signal assignment and selected signal assignment; consider the decoders outputs to be active-low.
 
 --           +------+
 -- d_in -/3->| deco |-/7-> d_out
@@ -7,15 +7,15 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity deco is
+entity exercise7 is
 port(
    d_in  : in  std_logic_vector(2 downto 0);
-   d_out : out std_logic_vector(7 downto 1)
+   d_out : out std_logic_vector(7 downto 0)
 );
 end;
 
 
-architecture deco_cond of deco is
+architecture deco_cond of exercise7 is
 begin
   d_out <= "11111110" when d_in = "000" else
            "11111101" when d_in = "001" else
@@ -25,10 +25,10 @@ begin
            "11011111" when d_in = "101" else
            "10111111" when d_in = "110" else
            "01111111" when d_in = "111" else
-           "11111111" when others;
+           "11111111";
 end;
 
-architecture deco_sel of deco is
+architecture deco_sel of exercise7 is
 begin
   with d_in select
    d_out <= "11111110" when "000",
